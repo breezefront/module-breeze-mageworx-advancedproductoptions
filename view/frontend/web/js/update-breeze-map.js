a76200ebed63d25e69a59bfc99fb3693b547acd9
Vitaliy Dmytruk <vitaliy@swissuplabs.com>
2022-11-04 14:51:30 +0200

    Prerender html templates with proper names and they will be resulved in `define`.

--- view/frontend/web/js/update-breeze-map.js
+++ view/frontend/web/js/update-breeze-map.js
@@ -13,17 +13,6 @@
             $.breezemap[component] = $.fn[component] && $.fn[component].bind($.fn)
     );
 
-    [
-        'dropdown',
-        'radio',
-        'checkbox',
-        'empty'
-    ].forEach(
-        (name) =>
-            $.breezemap[`text!MageWorx_OptionFeatures/template/option/gallery/${name}.html`] =
-                $(`#MageWorx_OptionFeatures_option_gallery_${name}`).html()
-    );
-
     if (!$.breezemap.uiRegistry) {
         $.breezemap.uiRegistry = {
             get: function (name) {

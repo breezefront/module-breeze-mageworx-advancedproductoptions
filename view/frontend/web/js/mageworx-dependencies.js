beae30c784b7d7c4bc08ea3e8052bc22384b71d0
Vitaliy Dmytruk <vitaliy@swissuplabs.com>
2022-11-04 10:31:57 +0200

    Breeze and `optionswatches` - fix error regarding mage('validation')

--- view/frontend/web/js/mageworx-dependencies.js
+++ view/frontend/web/js/mageworx-dependencies.js
@@ -65,4 +65,22 @@
             this._super();
         }
     });
+
+    if (!$.fn.mage) {
+        /**
+         * Copied form original Magento code
+         * magento/magento2-base/lib/web/mage/mage.js
+         */
+        $.fn.mage = function (component, config) {
+            config = config || {};
+
+            this.each(function (index, el) {
+                const $el = $(el);
+
+                $el[component].bind($el, config)
+            });
+
+            return this;
+        };
+    }
 })();

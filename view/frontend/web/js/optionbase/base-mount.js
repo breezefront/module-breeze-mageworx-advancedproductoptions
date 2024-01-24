0d8e8681fef2f9fc1970f042e8c663b7a399eb69
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-01-24 11:16:19 +0200

    Update to the latest module-breeze

--- view/frontend/web/js/optionbase/base-mount.js
+++
@@ -1,8 +0,0 @@
-/* global $ */
-(function () {
-    'use strict';
-
-    $(document).on('breeze:mount:optionBase', function (event, data) {
-        $(data.el).optionBase(data.settings);
-    });
-})();

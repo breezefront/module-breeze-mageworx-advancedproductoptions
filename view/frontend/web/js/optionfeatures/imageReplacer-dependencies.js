c5d14da96a5f1276a5a10be746527bdb9847bd5b
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-03-01 17:48:33 +0200

    Deferred is now added to breeze module

--- view/frontend/web/js/optionfeatures/imageReplacer-dependencies.js
+++ view/frontend/web/js/optionfeatures/imageReplacer-dependencies.js
@@ -2,22 +2,6 @@
 (function () {
     'use strict';
 
-    if (!$.Deferred) {
-        // https://stackoverflow.com/a/68851118
-        $.Deferred = () => {
-            function Defer() {
-                var promise = new Promise((resolve, reject) => {
-                    this.resolve = resolve;
-                    this.reject = reject;
-                });
-
-                this.promise = () => promise;
-            }
-
-            return new Defer();
-        };
-    }
-
     if (!$.when) {
         $.when = (promise) => {
             return promise;

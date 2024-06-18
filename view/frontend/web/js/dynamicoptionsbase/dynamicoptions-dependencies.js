ef8131b4919557ba83e016812cfb3810abe252ea
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2024-06-18 17:34:46 +0300

    Fix for the latest breeze module. Since modules are registered as anonymous when enabled bundling and "named" when bundling is disabled, we need to call register methods twice: first for the named module, second for the anonymous module :(

--- view/frontend/web/js/dynamicoptionsbase/dynamicoptions-dependencies.js
+++ view/frontend/web/js/dynamicoptionsbase/dynamicoptions-dependencies.js
@@ -1,5 +1,6 @@
 define(['uiComponent'], function (Component) {
     'use strict';
 
+    Component.register('dynamicOptionsDefaultCalculator', 'MageWorx_DynamicOptionsBase/js/calculator/default');
     Component.register('dynamicOptionsDefaultCalculator');
 });
--- view/frontend/web/js/optionfeatures/imageReplacer-register.js
+++ view/frontend/web/js/optionfeatures/imageReplacer-register.js
@@ -1,5 +1,6 @@
 define(['uiComponent'], function (Component) {
     'use strict';
 
+    Component.register('mwImageReplacer', 'MageWorx_OptionFeatures/js/swatches/imageReplacer');
     Component.register('mwImageReplacer');
 });

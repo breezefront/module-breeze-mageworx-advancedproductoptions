9d12f1c152ef07f09e59e9b116a01ea00e989166
Ibrahim Saleh <ibrahim@aqeltech.com>
2025-06-13 17:29:45 +0300

    Missing Components fix

---
+++ view/frontend/web/js/dynamicoptionsbase/dynamicoptions-register.js
@@ -0,0 +1,12 @@
+define(['uiComponent'], function (Component) {
+    'use strict';
+
+    Component.register('dynamicOptionMaxValueValidationRule', 'MageWorx_DynamicOptionsBase/js/validation/dynamicOptionMaxValueValidationRule');
+    Component.register('dynamicOptionMaxValueValidationRule');
+
+    Component.register('dynamicOptionMinValueValidationRule', 'MageWorx_DynamicOptionsBase/js/validation/dynamicOptionMinValueValidationRule');
+    Component.register('dynamicOptionMinValueValidationRule');
+
+    Component.register('dynamicOptionStepValidationRule', 'MageWorx_DynamicOptionsBase/js/validation/dynamicOptionStepValidationRule');
+    Component.register('dynamicOptionStepValidationRule');
+});
---
+++ view/frontend/web/js/optionfeatures/features-register.js
@@ -0,0 +1,6 @@
+define(['uiComponent'], function (Component) {
+  'use strict';
+
+  Component.register('selectionLimitValidationRule', 'MageWorx_OptionFeatures/js/selectionLimitValidationRule');
+  Component.register('selectionLimitValidationRule');
+});
